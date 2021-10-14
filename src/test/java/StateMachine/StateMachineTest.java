60e3234117a239584303643d66f42c6c84b62fdb
Manuel DESTOUESSE <manuel.destouesse@itrachealth.com>
2021-10-14 13:03:18 -0400

    First Commit

--- src/test/java/StateMachine/StateMachineTest.java
+++ src/test/java/StateMachine/StateMachineTest.java
@@ -10,22 +10,23 @@ class StateMachineTest {
     enum TestState implements IState {
         TEST_STATE_1,
         TEST_STATE_2,
-        TEST_STATE_3;
+        TEST_STATE_3
     }
     enum TestEvent implements IEvent {
         TEST_EVENT_1,
         TEST_EVENT_2,
-        TEST_EVENT_3;
+        TEST_EVENT_3
     }
-    Set<IState> testStates = new HashSet<IState>(Arrays.asList(
+    Set<IState> testStates = new HashSet<>(Arrays.asList(
             TestState.TEST_STATE_1,
             TestState.TEST_STATE_2,
             TestState.TEST_STATE_3));
 
-    Set<IEvent> testEvents = new HashSet<IEvent>(Arrays.asList(
+    Set<IEvent> testEvents = new HashSet<>(Arrays.asList(
             TestEvent.TEST_EVENT_1,
             TestEvent.TEST_EVENT_2,
             TestEvent.TEST_EVENT_3));
 
+    Set<ITransition> testTransitions;
 
 }
